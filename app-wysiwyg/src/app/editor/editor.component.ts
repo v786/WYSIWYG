6cd34f460008569704a6fb9a16a2ef443e5e88d9
v786 <jahangirali786@gmail.com>
2019-04-19 00:30:15 +0530

    Added get text button, CSS and hire me buttons

--- app-wysiwyg/src/app/editor/editor.component.ts
+++ app-wysiwyg/src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  @ViewChild('content') content;
+
+  innerHtml: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getContent(value) {
+    console.log(this.content.nativeElement + '!!!');
+    console.log(value.innerHTML + '!!!');
+    this.innerHtml = value.innerHTML;
   }
 
 }
